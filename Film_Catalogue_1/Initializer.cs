c13ab3abc3374d8206c120b6ccf7d89c26e4953d
Dmytro <165839721+MakDimas@users.noreply.github.com>
2024-04-11 12:35:00 +0300

    Add files via upload

---
+++ Film_Catalogue_1/Initializer.cs
@@ -0,0 +1,26 @@
+﻿using Film_Catalogue.DAL.Interfaces;
+using Film_Catalogue.DAL.Repositories;
+using Film_Catalogue.Domain.Entity;
+using Film_Catalogue.Service.Implementations;
+using Film_Catalogue.Service.Interfaces;
+
+namespace Film_Catalogue
+{
+    public static class Initializer
+    {
+        // Додавання репозиторіїв
+        public static void InitializeRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository<Film>, FilmRepository>();
+            services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
+            services.AddScoped<IBaseRepository<FilmCategory>, FilmCategoryRepository>();
+        }
+
+        // Додавання сервісів
+        public static void InitializeServices(this IServiceCollection services)
+        {
+            services.AddScoped<IFilmService, FilmService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+        }
+    }
+}
