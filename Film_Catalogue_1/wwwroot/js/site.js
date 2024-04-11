187afdb9046c7dcd41fa136c1df98ff98288884e
Dmytro <165839721+MakDimas@users.noreply.github.com>
2024-04-11 12:32:58 +0300

    Add files via upload

---
+++ Film_Catalogue_1/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿class CategorySelector {
+    constructor(categories, selectedCategories) {
+        this.categories = categories;
+        this.selectedCategories = selectedCategories;
+    }
+
+    //Метод для заповнення категорій
+    displayCategories() {
+        const selectElement = document.getElementById('categorySelect');
+
+        this.categories.forEach(category => {
+            const option = document.createElement('option');
+            option.value = category.id;
+            option.text = category.name;
+            option.selected = this.isCategorySelected(category.id);
+            selectElement.appendChild(option);
+        });
+    }
+
+    // Метод для вибору відмічених категорій
+    isCategorySelected(categoryId) {
+        return this.selectedCategories.includes(categoryId);
+    }
+}
